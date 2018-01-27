077bb6378498137a6669a60deaaa67385301b894
Joaquin Bello <juakob@gmail.com>
2018-01-27 14:33:14 -0300

    Joystick.hx

---
+++ src/io/Joystick.hx
@@ -0,0 +1,45 @@
+package io;
+import flixel.FlxG;
+import flixel.input.gamepad.FlxGamepad;
+
+/**
+ * ...
+ * @author Joaquin
+ */
+class Joystick implements PlayerInput
+{
+
+	var gamepad:FlxGamepad;
+	public function new(aGamepad:FlxGamepad) 
+	{
+		gamepad = aGamepad;
+	}
+	
+	/* INTERFACE io.PlayerInput */
+	
+	public function left():Bool 
+	{
+		return gamepad.analog.value.LEFT_STICK_X < -0.3;
+	}
+	
+	public function right():Bool 
+	{
+		return gamepad.analog.value.LEFT_STICK_X > 0.3;
+	}
+	
+	public function up():Bool 
+	{
+		return gamepad.analog.value.LEFT_STICK_Y < -0.3;
+	}
+	
+	public function down():Bool 
+	{
+		return gamepad.analog.value.LEFT_STICK_Y > .3;
+	}
+	
+	public function toss():Bool 
+	{
+		return gamepad.justPressed.A;
+	}
+	
+}
